0415870aa06b82c94922b63a5a8f863b6fb4a57c
FKNoyan <egemennoyan@hotmail.com>
2022-03-24 11:37:56 +0300

    almost ready

--- script.js
+++ script.js
@@ -20,9 +20,20 @@ const cardsContainer = document.querySelector(".cards-container");
 
 characters.map((character) => {
   // let characterTemplate = ``
-  let characterTemplate = ` <div class="card-container">
+  let pStyleWithAka = "margin-bottom: 0";
+  let divStyleWithAka =
+    "align-items: center; justify-content: end; flex-direction: column";
+  let characterTemplate = ` <div style="${
+    character.aka ? divStyleWithAka : ""
+  }" class="card-container">
   <img src="${character.image}" class="card-image" alt="" />
-  <p  class="w-text">${character.name}</p>
+  
+  <p style="${
+    character.aka ? pStyleWithAka : "margin-bottom: 40px"
+  }" class="w-text card-text">${character.name}</p>
+  <p style="margin-top: 0;"  class="w-text card-text">${
+    character.aka ? character.aka : ""
+  }</p>
   </div>`;
   // cardsContainerHtml;
   cardsContainer.innerHTML += characterTemplate;
